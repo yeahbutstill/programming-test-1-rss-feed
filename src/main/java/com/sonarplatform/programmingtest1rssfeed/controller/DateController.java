8d1285055bba76a81f94abb871d78ff11cbb67d6
dnl <mayadani28@gmail.com>
2024-06-02 14:20:19 +0700

    awesome @ResponseBody

--- src/main/java/com/sonarplatform/programmingtest1rssfeed/controller/DateController.java
+++ src/main/java/com/sonarplatform/programmingtest1rssfeed/controller/DateController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,15 +17,17 @@ public class DateController {
     // ini untuk formater response
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMyyyy");
 
-    @SneakyThrows
     @GetMapping("/date")
+    // ini untuk otomatis return value dari Type si methodnya menjadi isi body yang dituliskan ke HttpServletResponse
+    // cukup tambahkan annotation
+    @ResponseBody
     // disini karena kita sudah membuat coverternya di package util yang bernama classnya StringToDateConverter
     // nanti secara otomatis Spring akan mencari converter yang bisa melakukan coversi dari Tipe data String dari RequestParam ke Date
     // jadi nanti kalau dikirim, format requestnya harus kaya yang ada di Class StringToDateConverter
     // perlu diingat juga tipe data yang dasar seperti String, Integer, Boolean, Long itu semuanya sudah di conversi otomatis
     // menggunakan Spring Converter
-    public void getDate(@RequestParam(name = "date") Date date, HttpServletResponse httpServletResponse) {
+    public String getDate(@RequestParam(name = "date") Date date) {
         // balikan tanggal yang diterima tapi formatnya menggunakan format yang berbeda dari si dateFormat ini
-        httpServletResponse.getWriter().println("Date: " + dateFormat.format(date));
+        return "Date: " + dateFormat.format(date);
     }
 }
